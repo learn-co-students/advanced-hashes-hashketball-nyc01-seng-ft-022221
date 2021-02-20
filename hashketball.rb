20cced643b675a32e8c65e7860eb68df16378026
Henry Velasquez <hvelas93@gmail.com>
2021-02-20 02:59:21 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,6 @@
 # Write your code below game_hash
+
+require 'pry'
 def game_hash
   {
     home: {
@@ -126,4 +128,151 @@ def game_hash
   }
 end
 
+# final_results=data.each_with_object({}) do |(key,value), final_array|
+#         #binding.pry
+#         value.each do |inner_key,names|
+#           names.each do |name|
+#             #binding.pry
+#             if !final_array[name]
+#               final_array[name]={}
+#               #final_array<<name
+#             end
+#             if !final_array[name][key]
+#               !final_array[name][key]=[]
+#             end
+#         final_array[name][key].push(inner_key.to_s)
+#           end
+#        end
+#      end
+
 # Write code here
+
+def num_points_scored(player_name)
+
+scored_points=""
+
+    game_hash[:home][:players].each do |index|
+      if index[:player_name]==player_name
+        scored_points=index[:points]
+      end
+
+    end
+
+    game_hash[:away][:players].each do |index2|
+      if index2[:player_name]==player_name
+        scored_points=index2[:points]
+      end
+
+    end
+scored_points
+#binding.pry
+end
+
+def shoe_size(player_name2)
+
+  shoe_sizes=""
+
+      game_hash[:home][:players].each do |index|
+        if index[:player_name]==player_name2
+          shoe_sizes=index[:shoe]
+        end
+
+      end
+
+
+      game_hash[:away][:players].each do |index2|
+        if index2[:player_name]==player_name2
+          shoe_sizes=index2[:shoe]
+        end
+        #binding.pry
+end
+shoe_sizes
+end
+
+def team_colors(team_name)
+
+team_coloring=""
+
+if game_hash[:home][:team_name]==team_name
+  team_coloring=game_hash[:home][:colors]
+end
+
+  if game_hash[:away][:team_name]==team_name
+    team_coloring=game_hash[:away][:colors]
+  end
+
+team_coloring
+end
+
+def team_names
+teams_playing=Array.new
+  teams_playing<< game_hash[:home][:team_name]
+  teams_playing<< game_hash[:away][:team_name]
+
+end
+
+def player_numbers(team_name)
+
+  player_number_collection=[]
+
+      game_hash[:home][:players].each do |index|
+        if game_hash[:home][:team_name]==team_name
+          player_number_collection << index[:number]
+        end
+
+      end
+
+      game_hash[:away][:players].each do |index2|
+        if game_hash[:away][:team_name]==team_name
+          player_number_collection << index2[:number]
+        end
+
+      end
+  player_number_collection
+  #binding.pry
+  end
+
+  def player_stats(player_name)
+
+    player_statistics=""
+
+        game_hash[:home][:players].each do |index|
+          if index[:player_name]==player_name
+            player_statistics=index
+          end
+
+        end
+
+        game_hash[:away][:players].each do |index2|
+          if index2[:player_name]==player_name
+            player_statistics=index2
+          end
+
+        end
+    player_statistics
+    #binding.pry
+    end
+
+def big_shoe_rebounds
+
+shoe_sizes=[]
+corresponding_number_of_rebounds=[]
+
+player_statistics=""
+
+    game_hash[:home][:players].each do |index|
+      shoe_sizes<<index[:shoe]
+      corresponding_number_of_rebounds<<index[:rebounds]
+
+    end
+
+    game_hash[:away][:players].each do |index2|
+      shoe_sizes<<index2[:shoe]
+      corresponding_number_of_rebounds<<index2[:rebounds]
+
+    end
+
+index_to_find=shoe_sizes.find_index(shoe_sizes.max)
+corresponding_number_of_rebounds[index_to_find]
+
+end
